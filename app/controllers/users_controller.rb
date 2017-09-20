209a42e743b1231dea6034e711c1a86ba8982dc8
Apichai Chenthanakij <badchen@yahoo.com>
2017-09-20 14:50:03 -0700

    added user login, new, create, authenticate

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    login(@user)
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :current_city, :password)
+  end
+end
