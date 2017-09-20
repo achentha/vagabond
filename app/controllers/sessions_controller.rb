f97e1031d1c7b3c9df8c0e6cc3aee2baa20626ff
Apichai Chenthanakij <badchen@yahoo.com>
2017-09-20 15:33:53 -0700

    user edit/update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,10 @@ class SessionsController < ApplicationController
       end
     end
 
+  def destroy
+    logout
+    redirect_to root_path
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :verify_user, only: [:edit, :update]
+  def index
+  end
 
   def new
     @user = User.new
@@ -14,11 +17,31 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
 
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      flash[:error] = @user.errors.full_messages.join(" ")
+      redirect_to user_path(@user)
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :current_city, :password)
   end
+
+  def verify_user
+    if current_user[:id].to_s != params[:id].to_s
+      flash[:notice] = "No permission to edit other user information"
+      redirect_to user_path
+    end
+  end
+
 end
