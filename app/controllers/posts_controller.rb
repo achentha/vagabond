3256bde852aa50f67b8fea3da4d0839b26158e02
ellismitchell <elligmitchell@gmail.com>
2017-09-22 09:37:10 -0700

    completed sprint 3 - initial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+	before_action :verify_logged_in, except: [:show]
+	before_action :find_and_verify_user, only: [:edit, :update, :destroy]
   def show
     @post = Post.find_by_id(params[:id])
   end
@@ -22,11 +23,10 @@ class PostsController < ApplicationController
   end
 
   def edit
-  	@post = Post.find(params[:id])
+  	
   end
 
   def update
-  	@post = Post.find(params[:id])
   	if @post.update post_params
   		redirect_to @post
   	else
@@ -36,7 +36,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-  	@post = Post.find(params[:id])
   	@post.delete
   	redirect_to current_user
   end
@@ -44,5 +43,20 @@ class PostsController < ApplicationController
   private
   def post_params
   	params.require(:post).permit(:title,:content,:user,:city)
+  end
+
+  def verify_logged_in
+  	if !current_user
+  		flash[:error] = "You must be logged in to create or modify posts"
+  		redirect_to City.find(params[:id]) and return
+  	end
+  end
+
+  def find_and_verify_user
+  	@post = Post.find(params[:id])
+  	if current_user != @post.user
+  		flash[:error] = "You don't have permission to modify other users' posts"
+  		redirect_to @post and return
+  	end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :city
   validates :title, :content, presence: true
+  validates :title, length: {maximum: 200}
   default_scope { order("created_at DESC") }
 end
