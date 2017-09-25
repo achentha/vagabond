fb9b481a3467738db5aa69d8a194d507293fae01
David <davjue@gmail.com>
2017-09-25 14:00:21 -0700

    tom is now an external url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/tom.jpg"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://gazettereview.com/wp-content/uploads/2016/06/myspce-tom-photo-300x300.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates :name, :current_city, presence: true
   validates :name, uniqueness: true
